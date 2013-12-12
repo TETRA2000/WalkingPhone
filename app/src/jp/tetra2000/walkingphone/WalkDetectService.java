9190961b4920f68e4bb9e355711acf61421ee145
TETRA2000 <sagittariusm25@gmail.com>
2013-12-12 12:41:42 +0900

    add sensor setup code

--- app/src/jp/tetra2000/walkingphone/WalkDetectService.java
+++ app/src/jp/tetra2000/walkingphone/WalkDetectService.java
@@ -1,16 +1,40 @@
 package jp.tetra2000.walkingphone;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 
 public class WalkDetectService extends Service {
-	public WalkDetectService() {
-	}
+	private SensorManager mSensorManager;
+	private Sensor mAccel;
 
+	@Override
+	public void onCreate() {
+		
+	}
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO: Return the communication channel to the service.
 		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	@Override
+	public int onStartCommand (Intent intent, int flags, int startId) {
+		// setup sensor
+		if(mSensorManager == null) {
+	        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+	        mAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	        if(mAccel == null) {
+	        	stopSelf();
+	        }
+		}
+		
+		
+		
+		return START_STICKY;
 	}
 }
