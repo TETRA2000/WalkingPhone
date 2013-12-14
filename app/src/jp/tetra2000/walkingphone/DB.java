ab1ee30934cd775db4427da33ad9ca59dfe9c453
TETRA2000 <sagittariusm25@gmail.com>
2013-12-14 15:31:50 +0900

    reduce getWritableDatabase calls

--- app/src/jp/tetra2000/walkingphone/DB.java
+++ app/src/jp/tetra2000/walkingphone/DB.java
@@ -8,16 +8,14 @@ import android.database.sqlite.SQLiteDatabase;
 public class DB {
 	public static final String NAME = "walk_db";
 	
-	private DBHelper mHelper;
+	private SQLiteDatabase mDb;
 	
 	public DB(Context context) {
-		mHelper = new DBHelper(context);
+		mDb = new DBHelper(context).getWritableDatabase();
 	}
 	
 	public void add(int year, int month, int day, int length) {
 		int oldLen = get(year, month, day);
-		
-		SQLiteDatabase db = mHelper.getWritableDatabase();
 		
 		String selection = "year="+year+" and month="+month+" and day="+day;		
 		
@@ -28,28 +26,23 @@ public class DB {
 			cv.put("year", year);
 			cv.put("month", month);
 			cv.put("day", day);
-			db.insert("walk", null, cv);
+			mDb.insert("walk", null, cv);
 		} else {
-			db.update("walk", cv, selection, null);
+			mDb.update("walk", cv, selection, null);
 		}
-				
-		db.close();
 	}
 	
 	public int get(int year, int month, int day) {
-		SQLiteDatabase db = mHelper.getReadableDatabase();
 		
 		String columns[] = {"length"};
 		String selection = "year="+year+" and month="+month+" and day="+day;
 		Cursor cursor =
-				db.query("walk", columns, selection, null, null, null, null);
+				mDb.query("walk", columns, selection, null, null, null, null);
 		
 		int len = 0;
 		if(cursor.moveToFirst()) {
 			len = cursor.getInt(cursor.getColumnIndex("length"));
 		}
-		
-		db.close();
 		
 		return len;
 	}
