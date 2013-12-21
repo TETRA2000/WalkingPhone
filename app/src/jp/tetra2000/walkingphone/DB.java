35da8f6232df10bea63de7f6d731a2515690d565
TETRA2000 <sagittariusm25@gmail.com>
2013-12-21 18:22:40 +0900

    Revert "reduce getWritableDatabase calls"
    This reverts commit ab1ee30934cd775db4427da33ad9ca59dfe9c453.

--- app/src/jp/tetra2000/walkingphone/DB.java
+++ app/src/jp/tetra2000/walkingphone/DB.java
@@ -8,14 +8,16 @@ import android.database.sqlite.SQLiteDatabase;
 public class DB {
 	public static final String NAME = "walk_db";
 	
-	private SQLiteDatabase mDb;
+	private DBHelper mHelper;
 	
 	public DB(Context context) {
-		mDb = new DBHelper(context).getWritableDatabase();
+		mHelper = new DBHelper(context);
 	}
 	
 	public void add(int year, int month, int day, int length) {
 		int oldLen = get(year, month, day);
+		
+		SQLiteDatabase db = mHelper.getWritableDatabase();
 		
 		String selection = "year="+year+" and month="+month+" and day="+day;		
 		
@@ -26,23 +28,28 @@ public class DB {
 			cv.put("year", year);
 			cv.put("month", month);
 			cv.put("day", day);
-			mDb.insert("walk", null, cv);
+			db.insert("walk", null, cv);
 		} else {
-			mDb.update("walk", cv, selection, null);
+			db.update("walk", cv, selection, null);
 		}
+				
+		db.close();
 	}
 	
 	public int get(int year, int month, int day) {
+		SQLiteDatabase db = mHelper.getReadableDatabase();
 		
 		String columns[] = {"length"};
 		String selection = "year="+year+" and month="+month+" and day="+day;
 		Cursor cursor =
-				mDb.query("walk", columns, selection, null, null, null, null);
+				db.query("walk", columns, selection, null, null, null, null);
 		
 		int len = 0;
 		if(cursor.moveToFirst()) {
 			len = cursor.getInt(cursor.getColumnIndex("length"));
 		}
+		
+		db.close();
 		
 		return len;
 	}
