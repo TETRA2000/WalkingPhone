88078efb93a8ff8681a22c13b715dcd1832e8df6
TETRA2000 <sagittariuam25@gmail.com>
2014-01-11 20:24:47 +0900

    add FCounter (not implemented)

---
+++ app/src/jp/tetra2000/walkingphone/FCounter.java
@@ -0,0 +1,20 @@
+package jp.tetra2000.walkingphone;
+
+public class FCounter
+{
+	public FCounter() {
+		
+	}
+	
+	public FCounter(long nanodelta) {
+		
+	}
+	
+	public void addData(float value, long nanosec) {
+		
+	}
+	
+	public float getFrequency() {
+		return 0f;
+	}
+}
