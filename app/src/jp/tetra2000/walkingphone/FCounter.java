7001f193065d0895638ba96969db030a62fce982
TETRA2000 <sagittariuam25@gmail.com>
2014-01-11 21:02:30 +0900

    implemented FCounrer

--- app/src/jp/tetra2000/walkingphone/FCounter.java
+++ app/src/jp/tetra2000/walkingphone/FCounter.java
@@ -1,20 +1,40 @@
 package jp.tetra2000.walkingphone;
+import java.util.*;
+import java.nio.*;
 
 public class FCounter
 {
+	private long mDelta = 1000000000;
+	
+	private ArrayList<Long> mTimes = new ArrayList<Long>();
+	
+	private float mLastValue;
+	
 	public FCounter() {
 		
 	}
 	
 	public FCounter(long nanodelta) {
-		
+		mDelta = nanodelta;
 	}
 	
 	public void addData(float value, long nanosec) {
+		if(mLastValue>0 != value>0) {
+			// sign changed
+			mTimes.add(nanosec);
+		}
 		
+		mLastValue = value;
+		
+		updateList(nanosec);
 	}
 	
 	public float getFrequency() {
-		return 0f;
+		return (float)mTimes.size() / mDelta;
+	}
+	
+	private void updateList(long nowtime) {
+		while(nowtime - mTimes.get(0) < mDelta)
+			mTimes.remove(0);
 	}
 }
